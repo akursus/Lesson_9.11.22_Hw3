aa1462259e2ba69b0453b70e10c43d59f8967738
akursus <ak_ursus@list.ru>
2022-11-14 22:02:07 +0300

    task2

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,34 @@ public class Main {
         System.out.println("Значение переменной coordinateX с типом float равно " +coordinateX);
         double coordinateY = 12.123456789;
         System.out.println("Значение переменной coordinateY с типом double равно " +coordinateY);
+
+        //task2
+        System.out.println("Задание 2");
+        float meaning1 = 27.12f;
+        long meaning2 = 987678965549L;
+        byte meaning3 = 2;
+        short meaning4 = 786;
+        boolean meaning5 = meaning3 >= meaning4;
+        short meaning6 = 569;
+        short meaning7 = -159;
+        short meaning8 = 27897;
+        byte meaning9 = 67;
+        System.out.println(meaning1);
+        System.out.println(meaning2);
+        System.out.println(meaning3);
+        System.out.println(meaning4);
+        System.out.println(meaning5);
+        System.out.println(meaning6);
+        System.out.println(meaning7);
+        System.out.println(meaning8);
+        System.out.println(meaning9);
+        int meaning10 = 153897;
+        double meaning11 = 33.33333333334;
+        char meaning12 = 37;
+        System.out.println(meaning10);
+        System.out.println(meaning11);
+        System.out.println(meaning12);
+
+
     }
 }
