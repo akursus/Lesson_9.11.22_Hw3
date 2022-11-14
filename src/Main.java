32e7fc9cc1fa4096b8ffb1e2f75677afc3c1129a
akursus <ak_ursus@list.ru>
2022-11-14 20:46:04 +0300

    task1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        System.out.println("Задание 1");
+        int leftSocks = 15;
+        System.out.println("Значение переменной leftSocks с типом int равно " +leftSocks);
+        byte rightSocks = 18;
+        System.out.println("Значение переменной rightSocks с типом byte равно " +rightSocks);
+        short redHat = 93;
+        System.out.println("Значение переменной redHat с типом short равно " +redHat);
+        long christmasTree = 357;
+        System.out.println("Значение переменной christmasTree с типом long равно " +christmasTree);
+        float coordinateX = 45.12345f;
+        System.out.println("Значение переменной coordinateX с типом short равно " +coordinateX);
+        double coordinateY = 12.123456789;
+        System.out.println("Значение переменной coordinateY с типом short равно " +coordinateY);
+    }
+}
